8864306c8455226b486ca823f066afd98e995576
olezhka0809 <olezhka.garnautan@gmail.com>
2024-04-06 17:27:04 +0300

    Am adaugat citirea de la linia de intrare a denumirii directorului

--- project.c
+++ project.c
@@ -1,49 +1,61 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <time.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(argc != 2){
+        printf("Introduceti denumirea unui director \n");
+        return 1;
+    }
     DIR *folder;
     struct dirent *entry;
     struct stat filestat;
-    char cwdpath[256];
     int count = 1;
-    long long total = 0;
+    off_t total = 0;
 
-    folder = opendir(".");
+    folder = opendir(argv[1]);
     if(folder == NULL)
     {
         perror("Unable to read directory");
         return(1);
     }
 
-    getcwd(cwdpath,256);
-    printf(" Directory of %s\n\n",cwdpath);
+    printf("Directory of %s\n\n",argv[1]);
 
     while( (entry=readdir(folder)) )
     {
-        stat(entry->d_name,&filestat);
+        char filepath[512];
+        sprintf(filepath ," %s/%s ", argv[1], entry->d_name);
+
+        if(strcmp(entry->d_name, "." )==0 || strcmp(entry->d_name , "..") == 0)
+            continue;
+        stat(filepath ,&filestat);
+        //     perror("Unable to get file status");
+        //     return 1;
+        // }
+
         printf("%-16s",entry->d_name);
 
         if( S_ISDIR(filestat.st_mode) )
             printf("%-8s  ","<DIR>");
         else
         {
-            printf("%8lld  ",filestat.st_size);
+            printf("%ld  ",filestat.st_size);
             total+=filestat.st_size;
         }
 
         printf("%s",ctime(&filestat.st_mtime));
+        count++;
     }
 
     closedir(folder);
-    printf("\n        %d File(s) for %lld bytes\n",
-            count,
-            total
-          );
+    printf("\n%d File(s) for %ld bytes\n", count, total);
 
     return(0);
 }
