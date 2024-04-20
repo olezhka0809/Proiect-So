218f335f96e2b722730e3f45e465628c489ab123
olezhka0809 <olezhka.garnautan@gmail.com>
2024-04-20 20:02:54 +0300

    Rezolvarea conditiei pentru saptamana 7

--- project.c
+++ project.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,7 +8,9 @@
 #include <fcntl.h>
 #include <time.h>
 
-void listare_director(const char *dirname) {
+#define BUFFER_SIZE 1024
+
+void listare_director(const char *dirname, const char *output_dir) {
     DIR *dir;
     struct dirent *entry;
     struct stat info;
@@ -21,6 +22,7 @@ void listare_director(const char *dirname) {
 
     while ((entry = readdir(dir)) != NULL) {
         char path[512];
+        const char *filename = "SnapshotSubDir.txt";
         sprintf(path, "%s/%s", dirname, entry->d_name);
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
             continue;
@@ -31,45 +33,59 @@ void listare_director(const char *dirname) {
             continue;
         }
         if (S_ISDIR(info.st_mode)) {
-            char output_Snap[512];
-            printf("Tip : Director \n");
-            printf("Data ultimei modificari: %s\n", ctime(&info.st_mtime));
-            printf("\n");
-
-            sprintf(output_Snap, "%s/%s/SnapshotSubDir.txt", dirname, entry->d_name);
-            int file = open(output_Snap, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (file == -1) {
-                perror("Nu s-a putut crea fișierul!");
-                return;
+            if (stat(filename, &info) == 0) {
+                char output_snp2[512];
+                printf("Tip : Director \n");
+                printf("Data ultimei modificari: %s\n", ctime(&info.st_mtime));
+                printf("\n");
+                sprintf(output_snp2, "%s/%s/Snapshot2SubDir.txt", dirname, entry->d_name);
+                int file = open(output_snp2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (file == -1) {
+                    perror("Nu s-a putut crea fișierul!");
+                    return;
+                }
+                char data_modificare[512];
+                sprintf(data_modificare, "Data ultimei modificari: %s\n\n", ctime(&info.st_mtime));
+                write(file, "Tip : Director ", strlen("Tip : Director "));
+                write(file, entry->d_name, strlen(entry->d_name));
+                write(file, "\n", 1);
+                write(file, data_modificare, strlen(data_modificare));
+                close(file);
+                listare_director(path, output_dir);
+            } else {
+                char output_Snap[512];
+                printf("Tip : Director \n");
+                printf("Data ultimei modificari: %s\n", ctime(&info.st_mtime));
+                printf("\n");
+                sprintf(output_Snap, "%s/%s/SnapshotSubDir.txt", dirname, entry->d_name);
+                int file = open(output_Snap, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (file == -1) {
+                    perror("Nu s-a putut crea fișierul!");
+                    return;
+                }
+                char data_modificare[512];
+                sprintf(data_modificare, "Data ultimei modificari: %s\n\n", ctime(&info.st_mtime));
+                write(file, "Tip : Director ", strlen("Tip : Director "));
+                write(file, entry->d_name, strlen(entry->d_name));
+                write(file, "\n", 1);
+                write(file, data_modificare, strlen(data_modificare));
+                close(file);
+                listare_director(path, output_dir);
             }
-
-            char data_modificare[512];
-            sprintf(data_modificare, "Data ultimei modificari: %s\n\n", ctime(&info.st_mtime));
-            write(file, "Tip : Director ", strlen("Tip : Director "));
-            write(file, entry->d_name, strlen(entry->d_name));
-            write(file, "\n", 1);
-            write(file, data_modificare, strlen(data_modificare));
-
-            close(file);
-            listare_director(path);
         } else {
             char SnapshotFisiere[512];
             printf("Tip: Fisier\n");
             printf("Dimensiune: %ld bytes\n", info.st_size);
             printf("Data ultimei modificari: %s\n", ctime(&info.st_mtime));
             printf("\n");
-
             char numeFisier[512];
             sprintf(numeFisier, "%s-SnapshotFisier.txt", entry->d_name);
-
             sprintf(SnapshotFisiere, "%s/%s", dirname, numeFisier);
-
             int file2 = open(SnapshotFisiere, O_WRONLY | O_CREAT | O_TRUNC, 0644);
             if (file2 == -1) {
-                perror("Eroare la crearea snapshotului de fișiere!");
+                perror("Eroare la crearea snapshotului de fisiere!");
                 continue;
             }
-
             char data_modificare[512];
             sprintf(data_modificare, "Data ultimei modificari: %s\n\n", ctime(&info.st_mtime));
             write(file2, "Tip: Fisier\n", strlen("Tip: Fisier\n"));
@@ -79,18 +95,76 @@ void listare_director(const char *dirname) {
             write(file2, size_str, strlen(size_str));
             write(file2, " bytes\n", strlen(" bytes\n"));
             write(file2, data_modificare, strlen(data_modificare));
-
             close(file2);
         }
     }
     closedir(dir);
 }
 
+
+void update_snapshot(const char *dirname, const char *output_dir) {
+    // Check if the snapshot directory exists, create if not
+    DIR *dir = opendir(output_dir);
+    if (!dir) {
+        if (mkdir(output_dir) == -1) { // eliminam al doilea argument, permisiunile, care sunt specificate implicit
+            perror("mkdir");
+            return;
+        }
+    } else {
+        closedir(dir);
+    }
+
+    listare_director(dirname, output_dir);
+}
+
+
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Introduceți numele unui director \n");
+    if (argc < 3) {
+        printf("Utilizare: %s -o director_iesire dir1 [dir2 ...]\n", argv[0]);
         return 1;
     }
-    listare_director(argv[1]);
+
+    const char *output_dir = NULL;
+    int dirs_count = 0;
+    char **dirs = malloc((argc - 2) * sizeof(char *));
+    if (!dirs) {
+        perror("malloc");
+        return 1;
+    }
+
+    for (int i = 1; i < argc - 1; i++) {
+        if (strcmp(argv[i], "-o") == 0) {
+            if (i + 1 >= argc) {
+                printf("Argument lipsa pentru optiunea -o\n");
+                return 1;
+            }
+            output_dir = argv[i + 1];
+            i++;  // Skip the next argument (output directory)
+        } else {
+            dirs[dirs_count++] = argv[i];
+        }
+    }
+
+    if (dirs_count == 0) {
+        printf("Nu ati specificat niciun director.\n");
+        return 1;
+    }
+
+    if (output_dir == NULL) {
+        printf("Specificarea directorului de iesire este obligatorie folosind optiunea -o.\n");
+        return 1;
+    }
+
+    if (dirs_count > 10) {
+        printf("Nu puteti specifica mai mult de 10 directoare.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < dirs_count; i++) {
+        update_snapshot(dirs[i], output_dir);
+    }
+
+    free(dirs);
     return 0;
 }
