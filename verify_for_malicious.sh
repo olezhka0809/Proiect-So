156c13a0720d6314f24ffe67743a34e4da48c063
olezhka0809 <olezhka.garnautan@gmail.com>
2024-05-11 23:02:28 +0300

    Schimbam script-ul in dependenta de conditia saptamanii 10

--- verify_for_malicious.sh
+++ verify_for_malicious.sh
@@ -5,16 +5,37 @@ izolated_space_dir="$2"
 
 echo "Analizarea fișierului $file..."
 
-
 if [ ! -r "$file" ] || [ ! -w "$file" ] || [ ! -x "$file" ]; then
     echo "Fișierul $file are drepturi lipsă. Se va realiza analiza sintactică..."
 
-   
-    if grep -qE 'corrupted|dangerous|risk|attack|malware|malicious' "$file" || LC_ALL=C grep -q '[^ -~]' "$file"; then
+    
+    min_lines=3
+    max_words=1000
+    max_chars=2000
+
+    
+    line_count=$(wc -l < "$file")
+    word_count=$(wc -w < "$file")
+    char_count=$(wc -m < "$file")
+
+    
+    if (( line_count < min_lines )) && (( word_count > max_words )) && (( char_count > max_chars )); then
         echo "Fișierul $file este potențial periculos sau corupt. Va fi izolat în $izolated_space_dir."
         mv "$file" "$izolated_space_dir"
     else
-        echo "Fișierul $file nu prezintă semne de maleficență sau corupție."
+        
+        if LC_ALL=C grep -q '[^ -~]' "$file"; then
+            echo "Fișierul $file conține caractere non-ASCII. Este suspect și va fi izolat în $izolated_space_dir."
+            mv "$file" "$izolated_space_dir"
+        else
+            
+            if grep -qE 'corrupted|dangerous|risk|attack|malware|malicious' "$file"; then
+                echo "Fișierul $file conține cuvinte cheie asociate fișierelor periculoase. Este suspect și va fi izolat în $izolated_space_dir."
+                mv "$file" "$izolated_space_dir"
+            else
+                echo "Fișierul $file nu prezintă semne de maleficență sau corupție."
+            fi
+        fi
     fi
 else
     echo "Fișierul $file are toate drepturile și nu poate fi verificat."
