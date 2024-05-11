d51ad0e3025f793d2e82caf17ae35d854dc177e5
olezhka0809 <olezhka.garnautan@gmail.com>
2024-05-11 23:01:32 +0300

    Solutia saptaman 10

--- ProiectModificat.c
+++ ProiectModificat.c
@@ -10,13 +10,21 @@
 #include <sys/wait.h>
 
 #define MAX_DIRECTORIES 10
+#define MAX_CORRUPTED_FILES 100
 
-void analyze_file(const char *filename, const char *izolated_space_dir) {
+void analyze_file(const char *filename, const char *izolated_space_dir, int pipe_fd) {
     char command[1024];
     sprintf(command, "/home/kolah/Desktop/SOproject/ProiectSo/verify_for_malicious.sh %s %s", filename, izolated_space_dir);
     int status = system(command);
     if (status != 0) {
         fprintf(stderr, "Eroare la verificarea fișierului %s\n", filename);
+        
+        write(pipe_fd, filename, strlen(filename) + 1); 
+    }
+    else {
+        
+        char safe_msg[] = "SAFE\n";
+        write(pipe_fd, safe_msg, strlen(safe_msg) + 1); 
     }
 }
 
@@ -42,7 +50,7 @@ void create_snapshot(const char *name, const char *output_dir, const struct stat
     close(snapshot_fd);
 }
 
-void update_snapshot(const char *dirname, const char *output_dir, const char *izolated_space_dir) {
+void update_snapshot(const char *dirname, const char *output_dir, const char *izolated_space_dir, int *corrupted_files_count, char corrupted_file_names[MAX_CORRUPTED_FILES][256], int pipe_fd) {
     DIR *dir;
     struct dirent *entry;
     struct stat info;
@@ -68,25 +76,25 @@ void update_snapshot(const char *dirname, const char *output_dir, const char *iz
         }
 
         if (S_ISDIR(info.st_mode)) {
-            // Creează un proces copil pentru a explora subdirectorul
+            
             child_pid = fork();
             if (child_pid == -1) {
                 perror("fork");
                 exit(EXIT_FAILURE);
             } else if (child_pid == 0) {
-                // Proces copil
-                update_snapshot(path, output_dir, izolated_space_dir);
+                
+                update_snapshot(path, output_dir, izolated_space_dir, corrupted_files_count, corrupted_file_names, pipe_fd);
                 exit(EXIT_SUCCESS);
             } else {
-                // Așteaptă ca procesul copil să se încheie înainte de a continua
+               
                 wait(NULL);
             }
-            // Creează snapshot pentru subdirector
+            
             create_snapshot(entry->d_name, output_dir, &info);
         } else if (S_ISREG(info.st_mode)) {
-            // Verifică dacă fișierul este corupt
-            analyze_file(path, izolated_space_dir);
-            // Creează snapshot doar pentru fișierele necorupte
+            
+            analyze_file(path, izolated_space_dir, pipe_fd);
+            
             if (access(path, F_OK) == 0) {
                 create_snapshot(entry->d_name, output_dir, &info);
             }
@@ -110,21 +118,55 @@ int main(int argc, char *argv[]) {
     const char *output_dir = argv[2];
     const char *izolated_space_dir = argv[4];
 
+    int parent_to_child_pipe[2];
+    if (pipe(parent_to_child_pipe) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
     for (int i = 5; i < argc; i++) {
         pid_t pid = fork();
         if (pid == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
-            update_snapshot(argv[i], output_dir, izolated_space_dir);
+            
+            close(parent_to_child_pipe[0]); 
+
+            update_snapshot(argv[i], output_dir, izolated_space_dir, NULL, NULL, parent_to_child_pipe[1]);
             exit(EXIT_SUCCESS);
         }
     }
 
+    close(parent_to_child_pipe[1]); 
+
+    
+    int corrupted_files_count = 0;
+    char corrupted_file_names[MAX_CORRUPTED_FILES][256]; 
+    char buffer[256];
+    while (read(parent_to_child_pipe[0], buffer, sizeof(buffer)) > 0) {
+        if (strcmp(buffer, "SAFE") != 0) {
+            
+            strcpy(corrupted_file_names[corrupted_files_count], buffer);
+            corrupted_files_count++;
+        }
+    }
+
+    close(parent_to_child_pipe[0]); 
+
     int status;
     pid_t wpid;
     while ((wpid = wait(&status)) > 0) {
-        printf("Procesul copil cu PID-ul %d s-a încheiat cu codul de ieșire %d.\n", wpid, WEXITSTATUS(status));
+        printf("Procesul copil cu PID-ul %d s-a încheiat cu codul de ieșire %d", wpid, WEXITSTATUS(status));
+        if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS) {
+            printf(" și cu %d fișiere corupte.\n", corrupted_files_count);
+            for (int i = 0; i < corrupted_files_count; i++) {
+                printf("%s ", corrupted_file_names[i]);
+            }
+            printf("\n");
+        } else {
+            printf(" și fără fișiere corupte.\n");
+        }
     }
 
     return 0;
